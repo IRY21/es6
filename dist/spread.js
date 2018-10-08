2376671b3c7d8c9faf935b05dd803bc3f541349a
IRY21 <rilok21work@gmail.com>
2018-10-08 11:29:04 +0500

    spread-end

--- dist/spread.js
+++ dist/spread.js
@@ -1,1 +1,13 @@
 "use strict";
+
+var staticLanguages = ['C', 'C++', 'Java'];
+var dinamicLanguages = ['JavaScript', 'PHP', 'Ruby'];
+var languages = staticLanguages.concat(['C#'], dinamicLanguages, ['Python']);
+console.log(languages);
+
+function add(x, y, z) {
+  console.log(x + y + z);
+}
+
+var numbers = [1, 2, 3];
+add.apply(void 0, numbers);
