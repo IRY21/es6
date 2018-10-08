9c37d9b33a25d0c0f2922334fdf67de8453b5c0b
IRY21 <rilok21work@gmail.com>
2018-10-08 11:45:37 +0500

    template-string-end

--- dist/template-string.js
+++ dist/template-string.js
@@ -1,1 +1,37 @@
 "use strict";
+
+function _templateObject() {
+  var data = _taggedTemplateLiteral(["Hello ", ""]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+function greet(name) {
+  console.log("Hello ".concat(name).toUpperCase());
+}
+
+greet('Bill');
+
+function createEmail(to, from, subject, message) {
+  console.log("\n        To: ".concat(to, "\n        From: ").concat(from, "\n        Subject: ").concat(subject, "\n        Message: ").concat(message, "\n    "));
+}
+
+createEmail('john@mail.com', 'julia@mail.com', 'Hello', 'How are you doing?');
+
+function add(x, y) {
+  console.log("".concat(x, " + ").concat(y, " = ").concat(parseInt(x) + parseInt(y)));
+}
+
+add('5', '7');
+var name = 'Bill';
+console.log(upperName(_templateObject(), name));
+
+function upperName(literals, value) {
+  return literals[0] + value.toUpperCase();
+}
