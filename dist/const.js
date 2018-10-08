f043953f3612d91791e3e24b1d93799732519959
IRY21 <rilok21work@gmail.com>
2018-10-08 11:13:56 +0500

    const-end

--- dist/const.js
+++ dist/const.js
@@ -1,11 +1,7 @@
 "use strict";
 
-function _readOnlyError(name) { throw new Error("\"" + name + "\" is read-only"); }
-
 var MATH = {
   PI: 3.14159
 };
-MATH = (_readOnlyError("MATH"), {
-  PI: 3
-});
+MATH.PI = 3;
 console.log(MATH.PI);
--- src/const.js
+++ src/const.js
@@ -2,8 +2,6 @@ const MATH = {
     PI: 3.14159
 }
 
-MATH = {
-    PI: 3
-}
+MATH.PI = 3;
 
 console.log(MATH.PI);
