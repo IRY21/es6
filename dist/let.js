5e479b0a5aa212cb0f180f8ce252469bee5a9852
IRY21 <rilok21work@gmail.com>
2018-10-08 11:03:24 +0500

    let-end

--- dist/let.js
+++ dist/let.js
@@ -1,1 +1,18 @@
 "use strict";
+
+var btns = document.querySelectorAll('button');
+
+var _loop = function _loop(i) {
+  btn = btns[i];
+  btn.innerText = i;
+
+  btn.onclick = function () {
+    console.log(i);
+  };
+};
+
+for (var i = 0; i < btns.length; i++) {
+  var btn;
+
+  _loop(i);
+}
